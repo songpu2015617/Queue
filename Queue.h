b045efefd10c2a75a3e5c3530b6ecde02193b277
songpu2015617 <songpu2015617@users.noreply.github.com>
2016-06-28 14:42:43 -0400

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -9,12 +9,13 @@
 #define __Queue__Queue__
 #define MAX_SIZE 101
 #include <stdio.h>
-
+// implement queue data structure with circular array 
 class Queue {
 private:
     int A[MAX_SIZE];
     int m_front, m_rear;
 public:
+// basic function for queue data structure 
     Queue():m_front(-1),m_rear(-1){};
     bool isEmpty();
     bool isFull();
